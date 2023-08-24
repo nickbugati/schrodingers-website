db63c6609ad52ef3e2b2b1d9e1bc1ad83aa1d0d1
nickbugati <nicholasgongee@aol.com>
2023-08-24 19:30:52 -0400

    Timer reset

--- lockdown.js
+++ lockdown.js
@@ -1,34 +1,29 @@
-const schrodingerLogic = true; // Set to true for production, false for development
+const schrodingerLogic = false; // Set to true for production, false for development
 let lockdownDuration = 60000; // 1 minute
 
 if (!schrodingerLogic) {
     lockdownDuration = 0; // Bypass the lockdown for development purposes
 }
 
+// Set a new lockdown timer immediately upon script execution
+localStorage.setItem('lockdownEnd', Date.now() + lockdownDuration);
+
+// Now fetch that value for checks
 const lockdownEnd = Number(localStorage.getItem('lockdownEnd'));
 
 console.log("Schrodinger Logic: ", schrodingerLogic);
 console.log("Lockdown Duration: ", lockdownDuration);
 console.log("Lockdown End from localStorage: ", lockdownEnd);
 
-function initiateLockdown() {
-    localStorage.setItem('lockdownEnd', Date.now() + lockdownDuration);
-    window.location.reload(); // Refresh the page to reflect the new state
-}
-
-console.log("Schrodinger Logic: ", schrodingerLogic);
-console.log("Lockdown Duration: ", lockdownDuration);
-
-if (!lockdownEnd) {
-    console.log("No lockdown end detected. Initiating lockdown.");
-    initiateLockdown();
-} else {
-    console.log("Existing lockdown end: ", new Date(lockdownEnd * 1));
+if (lockdownEnd) {
+    console.log("Existing lockdown end: ", new Date(lockdownEnd));
 
     if (Date.now() >= lockdownEnd) {
-        console.log("Lockdown expired. Navigating to home.html.");
+        console.log("Lockdown expired (which should be impossible). Navigating to home.html.");
         window.location.href = 'home.html';
     } else {
         console.log("Lockdown still active. Not navigating.");
     }
+} else {
+    console.log("No lockdown end detected. This should never happen as we always initiate a lockdown.");
 }
